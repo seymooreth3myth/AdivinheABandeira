b28affffc6210abf62496eb29e47f2bdfff7fd4c
personal <darkfabiotm@gmail.com>
2023-01-01 23:18:33 -0300

    :sparkles: feat [flag]: Add new images and screen to select flags

--- AdivinheABandeira/AdivinheABandeira/ContentView.swift
+++ AdivinheABandeira/AdivinheABandeira/ContentView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct ContentView: View {
     @State private var mostrarResultado = false
     @State private var tituloResultado = ""
+    @State private var resultado = 0
+    @State private var tentativas = 0
     
     @State private var paises = ["Estonia", "France", "Germany", "Ireland", "Italy", "Monaco", "Nigeria", "Poland", "Russia", "Spain", "UK", "USA"].shuffled()
     
@@ -19,56 +21,93 @@ struct ContentView: View {
         ZStack {
             RadialGradient(stops: [.init(color: .purple, location: 0.3), .init(color: .black, location: 0.3)], center: .top, startRadius: 300, endRadius: 700)
                 .ignoresSafeArea()
-            VStack {
-                Spacer()
-                Text("Advinhe a bandeira")
-                    .font(.largeTitle.weight(.bold))
-                    .foregroundColor(.black)
-                VStack(spacing: 15) {
-                    VStack {
-                        Text("Selecione a bandeira da")
-                            .foregroundStyle(.secondary)
-                            .font(.subheadline.weight(.heavy))
-                        Text(paises[respostaCorreta].uppercased())
-                            .foregroundColor(.white)
-                            .font(.largeTitle.weight(.semibold))
-                    }
-                    ForEach(0..<3) { number in
-                        Button {
-                            tocouNaBandeira(number)
-                        } label: {
-                            Image(paises[number].lowercased())
-                                .renderingMode(.original)
-                                .clipShape(Capsule())
-                                .shadow(radius: 5)
+            if tentativas < 8 {
+                VStack {
+                    Spacer()
+                    Text("Advinhe a bandeira")
+                        .font(.largeTitle.weight(.bold))
+                        .foregroundColor(.black)
+                    VStack(spacing: 15) {
+                        VStack {
+                            Text("Selecione a bandeira da")
+                                .foregroundStyle(.secondary)
+                                .font(.subheadline.weight(.heavy))
+                            Text(paises[respostaCorreta].uppercased())
+                                .foregroundColor(.white)
+                                .font(.largeTitle.weight(.semibold))
+                        }
+                        ForEach(0..<3) { number in
+                            Button {
+                                tocouNaBandeira(number)
+                            } label: {
+                                Image(paises[number].lowercased())
+                                    .renderingMode(.original)
+                                    .clipShape(Capsule())
+                                    .shadow(radius: 5)
+                            }
                         }
                     }
+                    .frame(maxWidth: .infinity)
+                    .padding(.vertical, 20)
+                    .background(.ultraThinMaterial)
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                    Spacer()
+                    Spacer()
+                    Text("Placar: \(resultado)")
+                        .foregroundColor(.purple)
+                        .font(.title.bold())
+                    Spacer()
                 }
-                .frame(maxWidth: .infinity)
-                .padding(.vertical, 20)
-                .background(.ultraThinMaterial)
-                .clipShape(RoundedRectangle(cornerRadius: 20))
-                Spacer()
-                Spacer()
-                Text("Placar: ??")
-                    .foregroundColor(.purple)
-                    .font(.title.bold())
-                Spacer()
+                .padding()
+            } else {
+                VStack {
+                    Spacer()
+                    Text("Advinhe a bandeira")
+                        .font(.largeTitle.weight(.bold))
+                        .foregroundColor(.black)
+                    VStack(spacing: 15) {
+                        VStack {
+                          
+                            Text("Resultado")
+                                .foregroundColor(.white)
+                                .font(.largeTitle.weight(.semibold))
+                            Text("Placar: \(resultado)")
+                                .foregroundColor(.white)
+                                .font(.largeTitle.weight(.semibold))
+                        }
+                        Button("Jogar novamente") {
+                            tentativas = 0
+                            resultado = 0
+                            tituloResultado = ""
+                            mostrarResultado = false
+                        }
+                        .buttonStyle(.bordered)
+                        
+                    }
+                    .frame(maxWidth: .infinity)
+                    .padding(.vertical, 20)
+                    .background(.ultraThinMaterial)
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                    Spacer()
+                    Spacer()
+
+                }
+                .padding()
             }
-            .padding()
+   
           
         }
         .alert(tituloResultado, isPresented: $mostrarResultado) {
             Button("Próximo", action: perguntar)
-        } message: {
-            Text("Seu resultado é ???")
         }
     }
     func tocouNaBandeira(_ number: Int) {
+        tentativas = tentativas + 1
         if number == respostaCorreta {
-            tituloResultado = "Acertou"
+            resultado += 1
+            tituloResultado = "Parabéns! você acertou"
         } else {
-            tituloResultado = "Errou"
+            tituloResultado = "Errou! Essa bandeira representa \(paises[number].uppercased())"
         }
         mostrarResultado = true
     }
